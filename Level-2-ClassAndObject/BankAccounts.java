64a2e43f9b868b210d01efdb5a02339459d67a89
Rishav <rishavhimself@gmail.com>
2025-01-16 15:59:58 +0530

    Question 2

---
+++ Level-2-ClassAndObject/BankAccounts.java
@@ -0,0 +1,72 @@
+// Import Scanner class from util package for user input
+import java.util.Scanner;
+// Create BankAccount Class 
+class BankAccount{
+	String accountHolder;
+	String accountNumber;
+	Long balance;
+	
+	BankAccount(String accountHolder, String accountNumber, Long balance){
+		this.accountHolder= accountHolder;
+		this.accountNumber= accountNumber;
+		this.balance=balance;
+	}
+	
+	void displayDetails(){
+		System.out.println("Current Balance :" +balance);
+	}
+	
+	void withdraw(Long money){
+		if(money>balance)System.out.println("Low Balance, Can't Withdraw");
+		else{
+			balance-=money;
+		}
+		displayDetails();
+	}
+	void deposit(Long money){
+		balance+=money;
+		displayDetails();
+	}
+	
+}
+
+public class BankAccounts{
+		public static void main(String args[]){
+			// Create a Scanner object for taking input
+			Scanner scanner = new Scanner(System.in);
+			
+			// Ask the user to enter the name of holder
+			System.out.println("Enter the account holder name");
+			String accountHolder= scanner.nextLine();
+			
+			// Ask the user to enter the account number
+			System.out.println("Enter the account number");
+			String accountNumber=scanner.nextLine();
+			
+			// Ask the user to enter the balance
+			System.out.println("Enter the balance");
+			Long balance=scanner.nextLong();
+	
+			// Object of BankAccount class
+			BankAccount bankAccout = new BankAccount(accountHolder,accountNumber,balance);
+
+			// Display
+			System.out.println("Enter 1 to current balance");
+			System.out.println("Enter 2 to deposit money");
+			System.out.println("Enter 3 to withdraw money");
+			int input=scanner.nextInt();
+			Long money=0l;
+			if(input<1&&input>3)System.out.println("Invaild Input");
+			else if(input!=1){
+				System.out.println("Enter the amount");
+				money=scanner.nextLong();
+			}
+			if(input==1)bankAccout.displayDetails();
+			else if(input==2)bankAccout.deposit(money);
+			if(input==3)bankAccout.withdraw(money);
+		
+			// Close the scanner
+			scanner.close();
+		}
+	}
+	
