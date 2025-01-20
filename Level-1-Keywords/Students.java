5651128f2de83a35680066ed3ecc710666b836c5
Rishav <rishavhimself@gmail.com>
2025-01-20 16:25:10 +0530

    Question 5

---
+++ Level-1-Keywords/Students.java
@@ -0,0 +1,96 @@
+// Import Scanner class from util package for user input
+import java.util.Scanner;
+// Create Student Class
+class Student{
+	static String universityName="ABC Institute of Technology";
+	static int totalStudents=0;
+	String name;
+	final String rollNumber;
+	double marks[]=new double[3];
+	String grades;
+	
+	
+	Student(String name, String rollNumber, double []marks){
+		this.name= name;
+		this.rollNumber= rollNumber;
+		this.marks=marks;
+		totalStudents++;
+	}
+	
+	public void displayDetails(){
+		System.out.println("Name of the student : "+name);
+		System.out.println("Roll number : "+rollNumber);
+		System.out.println("Grades : " +grades);
+	}
+	 public void updateGrades(String newGrades){
+		grades=newGrades;
+		displayDetails();
+	}
+	
+	public void calculateGarde(double []marks){
+		double totalMarks=marks[0]+marks[2]+marks[1];
+		double averageMarks=totalMarks/3;
+		if (averageMarks >= 80) {
+            grades = "A";
+        }
+		else if (averageMarks >= 70) {
+            grades = "B";
+        }
+		else if (averageMarks >= 60) {
+            grades = "c";
+        } 
+		else if (averageMarks >= 50) {
+            grades = "D";
+        }
+		else if (averageMarks >= 40) {
+			grades = "E";
+        }
+		else {
+            grades = "R";
+        }
+		displayDetails();
+	}
+
+	public void displayTotalStudents(){
+		System.out.println("Total number of students are :"+totalStudents);
+	}
+	
+}
+public class Students{
+		public static void main(String args[]){
+			
+			// Create a Scanner object for taking input
+			Scanner scanner = new Scanner(System.in);
+			
+			// Ask the user to enter the student name
+			System.out.println("Enter the Student name");
+			String name= scanner.nextLine();
+
+			// Ask the user to enter the roll number
+			System.out.println("Enter the Student roll number");
+			String rollNumber=scanner.nextLine();
+			
+			double [] marks=new double[3];
+			
+			// Ask the user to enter the marks
+			System.out.println("Enter the Marks");
+			for(int i=0;i<marks.length;i++)marks[i]=scanner.nextDouble();
+	
+			//Creating the object
+			Student student = new Student(name,rollNumber,marks);
+			
+			if(student instanceof Student){
+				
+				System.out.println("The given object is an instance of Employee Class ");
+				
+				//Method calling
+				student.calculateGarde(marks);
+				student.updateGrades("A+");
+
+
+			}
+			// Close the scanner
+			scanner.close();
+		}
+	}
+	
